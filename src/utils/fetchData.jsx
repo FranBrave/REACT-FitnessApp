5302ee5ca3d2dd0da5f35a7c7d4bfa16e2767e3c
Fran <valiente.fco@gmail.com>
2022-06-16 00:32:03 +0200

    add Detail, ExerciseCard, ExerciseVideos and modified some components

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -7,6 +7,14 @@ export const exerciseOptions = {
     }
   };
 
+ export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': '98cabe703fmsh5fa934d2d218867p1a426fjsn7a571311f556'
+    }
+  };
+
 
 export const fetchData = async (url, options) => {
 
