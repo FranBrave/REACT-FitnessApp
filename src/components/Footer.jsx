a3828de833acdf6d9ed052ddcf7e8a6908cefffd
Fran <valiente.fco@gmail.com>
2023-05-20 01:48:59 +0200

    edit footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,8 @@
 import React from "react";
-import { Box, Stack, Typography } from "@mui/material";
-import Logo from "../assets/images/Logo-1.png";
+import { Box, Typography } from "@mui/material";
 
 const Footer = () => (
   <Box mt="80px" bgcolor="#FFF3F4">
-    <Stack
-      gap="40px"
-      sx={{ alignItems: "center" }}
-      flexWrap="wrap"
-      px="40px"
-      pt="24px"
-    >
-      <img src={Logo} alt="logo" style={{ width: "200px", height: "41px" }} />
-    </Stack>
     <Typography
       variant="h5"
       sx={{ fontSize: { lg: "28px", xs: "20px" } }}
